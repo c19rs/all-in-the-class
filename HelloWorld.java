03594f35f1ab637e9a31038ac8cd44285c3d1c53
Ryo <c19rs@dalton.org>
2017-10-06 10:34:04 -0400

    Step 2

--- HelloWorld.java
+++ HelloWorld.java
@@ -1,7 +1,5 @@
-//there is an error in this file, please fix it.
-
 public class HelloWorld {
   public static void main(String[] args) {
-    System.out.println("Hello World")
+    System.out.println("Hello World");
   }
 }
